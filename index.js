d507ff337d10d4f6a846144ecc0b3b48dd9cc700
Antoine Bellec <Antoine Bellec>
2017-02-22 16:25:59 +0200

    loginPage

--- index.js
+++ index.js
@@ -15,26 +15,38 @@ app.get('/', function(req, res) {
     res.send("Hello world");
 });
 
+var users = [{
+        id: "txgw35",
+        username: "test",
+        password: "pwd"
+    },
+    {
+        id: "xvj2f2",
+        username: "john",
+        password: "doe"
+    }
+    ];
+
 var posts = [
         {
             id: 0,
             user: {
                 id: 1,
                 username: "dtrump",
-                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg" 
-            },                                                 
+                profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+            },
             image: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
             imageThumbnail: "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-            likes: 892, 
+            likes: 892,
             caption: "Always winning #elections",
-            tags: ['elections'],         
+            tags: ['elections'],
             comments: [
                 {
                     id: 0,
                     user: {
                         id: 2,
                         username: "POTUS"
-                    },                    
+                    },
                     comment: "You're never going to make it don #losing",
                     userRefs: [],
                     tags: ["losing"]
@@ -44,23 +56,42 @@ var posts = [
                     user: {
                         id: 3,
                         username: "HillaryC"
-                    },                    
+                    },
                     comment: "Damn right @POTUS",
                     userRefs: ["POTUS"],
-                    tags: []       
+                    tags: []
                 },
             ]
 
         }
     ]
 
+
 app.get('/posts/relevant', function(req, res) {
     res.json(posts);
 });
 
-app.get('/posts/:id', function(req, res) {    
+app.get('/posts/:id', function(req, res) {
     res.json(posts[req.params.id]);
 });
+
+app.post('/login', function(req,res){
+    console.log("test");
+    console.log(req.body);
+    var u = users.find(function(element){
+         return (element.username === req.body.username) && (element.password === req.body.password);
+    });
+
+    if(u !== undefined)
+    {
+        return res.json({id: u.id, username: u.username});
+    }
+    else
+    {
+        return res.sendStatus(401);
+    }
+})
+
 
 // start listening for incoming HTTP connections
 app.listen(app.get('port'), function() {
